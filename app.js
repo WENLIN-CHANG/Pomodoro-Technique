44f5c9ffae636303889c92713b2cbe68a4cb0bfe
WENLIN-CHANG <magic8405cigam6@gmail.com>
2024-11-14 23:10:21 +0800

    "Define primary functions: startTimer(), pauseTimer(), and resetTimer() for Pomodoro timer functionality"

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ let cycleCount = 0;
 let workTime = 25 * 60; // 25 分鐘 (以秒為單位)
 let breakTime = 5 * 60; // 5 分鐘 (以秒為單位)
 let currentTime = workTime;
+
+function startTimer() {
+    timer = setInterval(updateTimer, 1000);
+}
+
+function pauseTimer() {
+    clearInterval(timer);
+}
+
+function resetTimer() {
+    pauseTimer();
+    currentTime = isWorking ? workTime : breakTime;
+    updateTimerDisplay();
+    isWorking = true;
+    cycleCount = 0;
+    updateCycleCount();
+}
